5e32a9f2b27d4259434b1503aa2adc3f0e8d3802
mr-ema <ema.a.v.c2017@gmail.com>
2023-11-22 22:06:25 -0300

    add local sqlite db for plants

---
+++ src/app/upgrades/upgrades.type.ts
@@ -0,0 +1,5 @@
+export interface UpgradeStatement {
+  toVersion: number;
+  statements: string[];
+}
+
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  databaseNames: [
+    { name: "plant-wiki" },
+  ],
 };
