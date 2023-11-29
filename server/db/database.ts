dfa42abb7784ab9940f03397b9b7dcd8940bee7e
mr-ema <ema.a.v.c2017@gmail.com>
2023-11-29 10:46:21 -0300

    server: add search api call

--- server/db/database.ts
+++ server/db/database.ts
@@ -1,3 +1,4 @@
+import { Plant } from "@models/plant";
 import { Database } from "bun:sqlite";
 
 const db = new Database("plantify.sqlite", { create: true });
@@ -6,38 +7,46 @@ export function init() {
   db.run(`
     CREATE TABLE IF NOT EXISTS plant (
       id            INTEGER PRIMARY KEY AUTOINCREMENT,
-      imgUrl        VARCHAR NOT NULL,
+      img_url       VARCHAR NOT NULL,
       name          VARCHAR NOT NULL,
       description   TEXT NOT NULL
-    );`
+    );
+    `
   );
 
   // Move it to a separated file for testing purposes in the future
-  const isTableEmpty = fetchAll().length == 0;
+  const isTableEmpty = getAll().length == 0;
   if (Bun.env.ENVIROMENT == "develoment" && isTableEmpty) {
     const { MOCK_PLANTS } = require("@mock-data/plant.ts");
 
     for (const plant of MOCK_PLANTS) {
       db.run(
-        'INSERT INTO plant (imgUrl, name, description) VALUES (?, ?, ?)',
-        [plant.imgUrl, plant.name, plant.description]
+        'INSERT INTO plant (img_url, name, description) VALUES (?, ?, ?)',
+        [plant.img_url, plant.name, plant.description]
       );
     }
   }
 }
 
-export function fetchAll() {
-  const query = db.query("SELECT * FROM plant");
-  const plants = query.all();
-  query.finalize();
+export function getAll(): Plant[] | null[] {
+  const stmt = db.query("SELECT * FROM plant");
+  const result = stmt.all() as Plant[] | null[];
+  stmt.finalize();
 
-  return plants;
+  return result;
 }
 
-export function getPlantById(id: string | number) {
-  const query = db.query(`SELECT * FROM plant WHERE id = ${id};`);
-  const plants = query.all();
-  query.finalize();
+export function getPlantById(id: string | number): Plant | null {
+  const stmt = db.prepare("SELECT * FROM plant WHERE id = ?;");
+  const result = stmt.get(id) as Plant | null;
+  stmt.finalize();
 
-  return plants[0];
+  return result;
+}
+
+export function searchPlan(pattern: string): Plant[] | null[] {
+  const stmt = db.query(`SELECT * FROM plant WHERE name LIKE '%${pattern}%';`);
+  const result = stmt.all() as Plant[] | null[];
+
+  return result;
 }
--- server/main.ts
+++ server/main.ts
@@ -1,25 +1,35 @@
 import server from "bunrest";
 
 import * as db from "./db/database.ts";
-import { Plant } from "@models/plant.ts";
 
 const app = server();
 db.init();
 
-app.get('/api/v1/plants', (req, res) => {
-  const plants: [Plant | null] = db.fetchAll() as [Plant];
-
+app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
   res.setHeader("Access-Control-Allow-Origin", "*");
+
+  next?.();
+});
+
+app.get('/api/v1/plants', (req, res) => {
+  const plants = db.getAll();
+
   res.status(200).json(plants);
 });
 
 app.get('/api/v1/plants/:id', (req, res) => {
   const plant = db.getPlantById(req.params?.id);
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
   res.status(200).json(plant);
 });
 
-app.listen(Bun.env.PORT as string, () =>
-  console.log(`Example server listening on port ${Bun.env.PORT}!`),
+app.get('/api/v1/plants/search', (req, res) => {
+  const plant = db.getPlantById(req.params?.id);
+
+  res.status(200).json(plant);
+});
+
+app.listen(Bun.env?.PORT || 8000, () =>
+  console.log(`Example server listening on port ${Bun.env?.PORT || 8000}!`),
 );
