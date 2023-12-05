b015eb86570583f4b1eda5341d08f214a7df24fc
mr-ema <ema.a.v.c2017@gmail.com>
2023-12-05 13:08:47 -0300

    refactor: refactored server side code

--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,9 @@
 import server from "bunrest";
 
-import * as db from "./db/database.ts";
+import * as db from "@db/database.ts";
+import { user_routes } from "@routes/users.ts";
+import { plant_routes } from "@routes/plants.ts";
 
-// TODO: Split endpoints in separated files, ex. users.ts, plants.ts
 // TODO: Add security and data integrity validations
 // TODO: Correct messages and code status
 
@@ -28,123 +29,8 @@ app.use((req, res, next) => {
   next?.();
 });
 
-app.get("/api/v1/plants", (req, res) => {
-  const plants = db.getAll();
-
-  res.status(200).json(plants);
-});
-
-app.get("/api/v1/plants/search", (req, res) => {
-  const query = req.query?.query;
-
-  if (!query) {
-    return res.status(400).json({ error: "Bad request" });
-  }
-
-  const plant = db.searchPlant(query);
-  res.status(200).json(plant);
-});
-
-
-app.get("/api/v1/plants/:id", (req, res) => {
-  const plant = db.getPlantById(req.params?.id);
-
-  res.status(200).json(plant);
-});
-
-app.post("/api/v1/users/:user_id/bookmarks", (req, res) => {
-  const user_id = req.params?.user_id;
-  let { entity_id, entity_type } = req.body as { entity_id: string, entity_type: string };
-
-  if (!entity_type || !user_id || isNaN(user_id) || !entity_id || isNaN(entity_id)) {
-    return res.status(400).json({ error: "Invalid user id or entity id or entity type" });
-  }
-
-  entity_type = db.cleanEntityType(entity_type);
-  if (!db.isValidBookmarkEntityType(entity_type)) {
-    return res.status(400).json({ error: "Invalid entity type" });
-  }
-
-  const bookmark = db.getBookmarkForUser(user_id, entity_id, entity_type);
-  if (bookmark) {
-    return res.status(409).json({ error: 'Bookmark already exists for this user and entity' });
-  }
-
-  const new_bookmark = db.addBookmarkForUser(user_id, entity_id, entity_type);
-  res.status(201).json({ message: "Bookmark added successfully", bookmark: new_bookmark});
-});
-
-app.put("/api/v1/users/:user_id/bookmarks", (req, res) => {
-  const user_id = req.params?.user_id;
-  let { entity_type } = req.body as { entity_type: string };
-  const { entity_id, is_bookmarked } = req.body as { entity_id: string, is_bookmarked: boolean };
-
-  if (typeof is_bookmarked !== 'boolean') {
-    return res.status(400).json({ error: "Invalid is_bookmarked type" });
-  }
-
-  if (!entity_type || !user_id || isNaN(user_id) || !entity_id || isNaN(entity_id)) {
-    return res.status(400).json({ error: "Invalid user id or entity id or entity type" });
-  }
-
-  entity_type = db.cleanEntityType(entity_type);
-  if (!db.isValidBookmarkEntityType(entity_type)) {
-    return res.status(400).json({ error: "Invalid entity type" });
-  }
-
-  let bookmark = db.getBookmarkForUser(user_id, entity_id, entity_type)
-  if (!bookmark) {
-    return res.status(404).json({ error: 'Bookmark not found' });
-  }
-
-  const updated = db.toggleBookmarkForUser(user_id, entity_id, entity_type, is_bookmarked ? 1 : 0);
-  if (!updated) {
-    return res.status(400).json({ error: 'Failed to update bookmark' });
-  }
-
-  bookmark.is_bookmarked = is_bookmarked ? 1 : 0;
-  res.status(200).json({ message: 'Bookmark updated successfully', bookmark: bookmark });
-});
-
-app.get("/api/v1/users/:user_id/bookmarks/:entity_type", (req, res) => {
-  const user_id = req.params?.user_id;
-  let entity_type = req.params?.entity_type;
-
-  if (!entity_type || !user_id || isNaN(user_id)) {
-    return res.status(400).json({ error: "Invalid user id or entity type" });
-  }
-
-  entity_type = db.cleanEntityType(entity_type);
-  if (!db.isValidBookmarkEntityType(entity_type)) {
-    return res.status(400).json({ error: "Invalid entity type" });
-  }
-
-  const bookmarkedPlants = db.getAllBookmarkedPlantsForUser(user_id);
-
-  res.status(200).json(bookmarkedPlants);
-});
-
-app.get('/api/v1/users/:user_id/bookmarks/:entity_type/:entity_id', (req, res) => {
-  const user_id = req.params?.user_id;
-  const entity_id = req.params?.entity_id;
-  let entity_type = req.params?.entity_type;
-
-  if (!entity_type || !user_id || isNaN(user_id) || !entity_id || isNaN(entity_id)) {
-    return res.status(400).json({ error: "Invalid user id or plant id or entity type" });
-  }
-
-  entity_type = db.cleanEntityType(entity_type);
-  if (!db.isValidBookmarkEntityType(entity_type)) {
-    return res.status(400).json({ error: "Invalid entity type" });
-  }
-
-  const bookmark = db.getBookmarkForUser(user_id, entity_id, entity_type);
-  if (!bookmark) {
-    return res.status(404).json({ error: "Bookmarked Not Found" });
-  }
-
-  res.status(200).json({ bookmark: bookmark });
-});
+app.use('/api/v1/users', user_routes);
+app.use('/api/v1/plants', plant_routes);
 
 app.listen(Bun.env?.PORT || 8000, () =>
   console.log(`Example server listening on port ${Bun.env?.PORT || 8000}!`),
--- server/src/models/bookmark.ts
+++ server/src/models/bookmark.ts
@@ -1,5 +1,6 @@
 export interface Bookmark {
   id: number,
+  name: string,
   user_id: number,
   entity_id: number,
   is_bookmarked: number
